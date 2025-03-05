ed0dfddadd27251d54b857d53b6cce47c4e19799
Gerysx <geralvarez95rgmail.com>
2025-03-05 11:08:24 +0100

    Añadimos la redireccion a la vista resumeorden

--- src/main/java/com/curso/ecommerce/spring_ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/controller/HomeController.java
@@ -116,4 +116,10 @@ public class HomeController {
         model.addAttribute("orden", orden);
         return "usuario/carrito";
     }
+
+    @GetMapping("/order")
+    public String order(){
+        
+        return "usuario/resumenorden";
+    }
 }
