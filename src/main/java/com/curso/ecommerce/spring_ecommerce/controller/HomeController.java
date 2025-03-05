9f210b9bf5c8f4c87bacdf8eb6d62e9de7e785ee
Gerysx <geralvarez95rgmail.com>
2025-03-05 10:21:08 +0100

    Añaddir los productos al carrito de compras y mostrarlos en la vista carrito

--- src/main/java/com/curso/ecommerce/spring_ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/controller/HomeController.java
@@ -52,13 +52,27 @@ public class HomeController {
     }
 
     @PostMapping("/cart")
-    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad){
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model){
         DetalleOrden detalleOrden = new DetalleOrden();
         Producto producto = new Producto();
         Double sumaTotal = 0.0;
         Optional<Producto> optionalProducto= productoService.get(id);
         log.info("Producto añadido : {}", optionalProducto.get() );
         log.info("Cantidad: {}",cantidad);
+        producto=optionalProducto.get();
+        detalleOrden.setCantidad(cantidad);
+        detalleOrden.setPrecio(producto.getPrecio());
+        detalleOrden.setNombre(producto.getNombre());
+        detalleOrden.setTotal(producto.getPrecio()*cantidad);
+        detalleOrden.setProducto(producto);
+
+        detalles.add(detalleOrden);
+
+        sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+
+        orden.setTotal(sumaTotal);
         return "usuario/carrito";
     }
 }
