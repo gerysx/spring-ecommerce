9b1f7d68bcc46d711716c0e788eb12664a094812
Gerysx <geralvarez95rgmail.com>
2025-03-05 09:36:41 +0100

    mostrar informacion del producto en la pagina productohome

--- src/main/java/com/curso/ecommerce/spring_ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.curso.ecommerce.spring_ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.curso.ecommerce.spring_ecommerce.model.Producto;
 import com.curso.ecommerce.spring_ecommerce.service.ProductoService;
 
 @Controller
@@ -27,8 +30,12 @@ public class HomeController {
     }
 
     @GetMapping("productohome/{id}")
-    public String productoHome(@PathVariable Integer id){
+    public String productoHome(@PathVariable Integer id, Model model){
         log.info("ID producto enviado como parámetro{}", id);
+        Producto producto = new Producto();
+        Optional<Producto> productoOptional = productoService.get(id);
+        producto = productoOptional.get();
+        model.addAttribute("producto", producto);
         return "usuario/productohome";
     }
 }
